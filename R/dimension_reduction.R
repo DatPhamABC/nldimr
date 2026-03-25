8a64e7bc918bd027d11c87833fb2421e2ccd36e8
DatPhamABC <datpham5810@gmail.com>
2026-03-25 15:57:39 +0100

    fix 3d plot

--- R/dimension_reduction.R
+++ R/dimension_reduction.R
@@ -1,7 +1,7 @@
 library(R6)
 library(ggplot2)
-library(rgl)
-source('R/colors.R')
+library(plotly)
+# source('R/colors.R')
 
 
 Dimension_reduction <- R6Class(classname = "dimension reduction",
@@ -12,6 +12,7 @@ Dimension_reduction <- R6Class(classname = "dimension reduction",
                     isDistance = FALSE,
                     group = NULL,
 
+                    ############################################################
                     plotResult = function(){
                       tryCatch({
                         if (is.null(private$result)){
@@ -22,8 +23,9 @@ Dimension_reduction <- R6Class(classname = "dimension reduction",
                         if (ncol(private$result) != 2 & ncol(private$result) != 3){
                           stop(paste("Unable to visualize", ncol(private$result),
                                      "dimension. The number of dimensions must be 2 or 3."))
-                        } else if ( ncol(private$result) == 2 ) {
+                        }
 
+                        if ( ncol(private$result) == 2 ) {
                           full_data <- data.frame(private$result)
                           cols <- c('dim_1', 'dim_2')
 
@@ -50,30 +52,32 @@ Dimension_reduction <- R6Class(classname = "dimension reduction",
                           print(plt)
                           return(plt)
 
-                         } else {
-                           private$plot3d()
-                           }
+                         }
+
+                        if ( ncol(private$result) == 3 ) {
+                           private$plot3d(private$result, as.factor(self$group))
+                          }
                         },
 
                         # error occurs
-                        error=function(e) {
+                        error = function(e) {
                           message('Plot error:')
                           print(e)
                         }
                       )
                     },
 
-
+                    ############################################################
                     fitPlot = function(){
                       if (self$isDistance){
-                        full_dist = data.frame(as.vector(self$data),
-                                               as.vector(dist(private$result)))
+                        full_dist <- data.frame(as.vector(self$data),
+                                                as.vector(dist(private$result)))
                       } else {
-                        full_dist = data.frame(as.vector(dist(self$data)),
-                                               as.vector(dist(private$result)))
+                        full_dist <- data.frame(as.vector(dist(self$data)),
+                                                as.vector(dist(private$result)))
                       }
                       colnames(full_dist) <- c('high_dim', 'low_dim')
-                      plt = ggplot(data = full_dist, aes(x = high_dim, y = low_dim)) +
+                      plt <- ggplot(data = full_dist, aes(x = high_dim, y = low_dim)) +
                         geom_point(size = 2) +
                         labs(x = 'High-dimensional Distance',
                              y = 'Low-dimensional Distance') +
@@ -84,25 +88,25 @@ Dimension_reduction <- R6Class(classname = "dimension reduction",
 
                     },
 
-
+                    ############################################################
                     getJaccardSimilarity = function(k=5, method='euclidean'){
                       tryCatch({
                         if(is.null(k) | k>=nrow(self$data)){
-                          stop("Invalid number of neighbors.")
+                          stop("Invalid number of neighbors. The number of neighbors must be less than the number of observations.")
                         }
 
                         if(self$isDistance){
-                          high.dist = self$data
+                          high.dist <- self$data
                         } else {
-                          high.dist = as.matrix(dist(self$data, method = method))
+                          high.dist <- as.matrix(dist(self$data, method = method))
                         }
 
-                        low.dist = as.matrix(dist(private$result, method = method))
+                        low.dist <- as.matrix(dist(private$result, method = method))
 
-                        jaccard_sim = c()
+                        jaccard_sim <- c()
 
                         for (row in 1:nrow(self$data)){
-                          jaccard_sim = append(jaccard_sim,
+                          jaccard_sim <- append(jaccard_sim,
                                                private$jacc(as.vector(order(high.dist[row,])[2:(k+1)]),
                                                             as.vector(order(low.dist[row,])[2:(k+1)])))
                         }
@@ -116,6 +120,7 @@ Dimension_reduction <- R6Class(classname = "dimension reduction",
                       })
                     },
 
+                    ############################################################
                     plotJaccardSimilarity = function(k=5, method='euclidean') {
                       tryCatch({
                         if (is.null(private$result)){
@@ -126,12 +131,14 @@ Dimension_reduction <- R6Class(classname = "dimension reduction",
                         if (ncol(private$result) != 2 & ncol(private$result) != 3){
                           stop(paste("Unable to visualize Jaccard Similarity in", ncol(private$result),
                                      "dimension. The number of dimensions must be 2 or 3."))
-                        } else if ( ncol(private$result) == 2 ) {
+                        }
 
-                          jaccard <- self$getJaccardSimilarity(k, method)
-                          full_data <- data.frame(private$result, jaccard)
+
+                        jaccard <- self$getJaccardSimilarity(k, method)
+                        full_data <- data.frame(private$result, jaccard)
+
+                        if ( ncol(private$result) == 2 ) {
                           cols <- c('dim_1', 'dim_2', 'jaccard')
-
                           colnames(full_data) <- cols
 
                           plt <- ggplot(data = full_data) +
@@ -144,8 +151,12 @@ Dimension_reduction <- R6Class(classname = "dimension reduction",
                           print(plt)
                           return(plt)
 
-                        } else {
-                          private$plot3d()
+                        }
+
+                        if ( ncol(private$result) == 3) {
+                          cols <- c('dim_1', 'dim_2', 'dim_3')
+                          colnames(full_data) <- cols
+                          private$plot3d(full_data, jaccard)
                         }
 
                       },error = function(e) {
@@ -154,8 +165,15 @@ Dimension_reduction <- R6Class(classname = "dimension reduction",
                       })
                     },
 
-                    plotJaccardPerNeighbor = function(max_k=1000, method='euclidean'){
+                    ############################################################
+                    plotJaccardPerNeighbor = function(max_k, method='euclidean'){
                       result <- data.frame()
+
+                      if (max_k >= nrow(self$data)){
+                        message('The number of the maximum nearest neighbors cannot exceed the number of observations. Setting the maximum number of nearest neighbors to the number of observations - 1')
+                        max_k <- nrow(self$data) - 1
+                      }
+
                       for(k in c(1:max_k)){
                         result <- rbind(result, data.frame(rep(k, nrow(self$data)),
                                                            mean(self$getJaccardSimilarity(k, method))))
@@ -164,13 +182,17 @@ Dimension_reduction <- R6Class(classname = "dimension reduction",
                       colnames(result) <- c('k', 'jaccard_similarity')
 
                       plt <- ggplot(result, aes(x=k, y=jaccard_similarity)) +
-                        geom_line()
+                        geom_line() +
+                        labs(x = 'k', y = 'Average Jaccard Similarity')
 
                       print(plt)
                       return(plt)
                     }
 
                   ),
+
+
+
 
                   # private attributes and methods
                   private = list(
@@ -216,38 +238,40 @@ Dimension_reduction <- R6Class(classname = "dimension reduction",
                     #   return(plt)
                     # },
 
-                    plot3d = function(){
-                      full_result = private$result
-                      colnames(full_result) <- c('Dimension_1', 'Dimension_2',
-                                                 'Dimension_3')
+                    ############################################################
+                    plot3d = function(data, group = NULL){
+                      full_data <- data.frame(data)
+                      cols <- c('dim_1', 'dim_2', 'dim_3')
 
-                      # separate plot with group and without group
-                      if(is.null(self$group)){
-                        plot3d(x = private$result$Dimension_1,
-                               y = private$result$Dimension_2,
-                               z = private$result$Dimension_3,
-                               type = 'p',
-                               col = '#000000',
-                               size = 5,
-                               xlab = 'Dimension 1',
-                               ylab = 'Dimension 2',
-                               zlab = 'Dimension 3')
+                      if(!is.null(group)){
+                        full_data <- cbind(full_data, self$group)
+                        cols <- append(cols, 'group')
+                        colnames(full_data) <- cols
+
+                        fig <- plot_ly(full_data,
+                                       x = ~dim_1, y = ~dim_2, z = ~dim_3,
+                                       color = ~group)
                       } else {
-                        plot3d(x = private$result$Dimension_1,
-                               y = private$result$Dimension_2,
-                               z = private$result$Dimension_3,
-                               type = 'p',
-                               col = get_colors(self$group),
-                               size = 5,
-                               xlab = 'Dimension 1',
-                               ylab = 'Dimension 2',
-                               zlab = 'Dimension 3')
+                        colnames(full_data) <- cols
+
+                        fig <- plot_ly(full_data,
+                                       x = ~dim_1, y = ~dim_2, z = ~dim_3,
+                                       colors = c('#000'))
                       }
+
+                      fig <- fig %>% add_markers()
+                      fig <- fig %>% layout(scene = list(xaxis = list(title = 'Dimension 1'),
+                                                         yaxis = list(title = 'Dimension 2'),
+                                                         zaxis = list(title = 'Dimension 3')))
+
+                      return(fig)
+
                     },
 
+                    ############################################################
                     jacc = function(x, y) {
-                      intersection = length(intersect(x, y))
-                      union = length(x) + length(y) - intersection
+                      intersection <- length(intersect(x, y))
+                      union <- length(x) + length(y) - intersection
                       return (intersection/union)
                     }
 
